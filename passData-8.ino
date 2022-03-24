2860af89bfd5579046db4a2e8e7f0e43170f3811
Henk Oegema <henk@oegema.com>
2022-03-24 14:00:37 +0100

    Add files via upload

---
+++ passData-8.ino
@@ -0,0 +1,87 @@
+//Using an Arduino with Python LESSON 11: Controlling an LED from Python
+//https://www.youtube.com/watch?v=VdSFwYrYqW0&t=682s
+
+int redPin = 6;
+int greenPin = 5;
+int bluePin = 3;
+//Common ANODE = +5V
+
+int ON = 0;
+int OFF = 255;
+
+int redVal;
+int greenVal;
+int blueVal;
+
+String cmd;
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin,OUTPUT);
+  pinMode(bluePin,OUTPUT);
+}
+
+void loop() {
+  while(Serial.available() == 0) {}
+
+  redVal = Serial.readStringUntil(':').toInt();
+  greenVal = Serial.readStringUntil(':').toInt();
+  blueVal = Serial.readStringUntil('\r').toInt();
+
+Serial.println(redVal);
+Serial.println(greenVal);
+Serial.println(blueVal);
+
+  analogWrite(redPin, 255 - redVal);
+  analogWrite(greenPin, 255 - greenVal);
+  analogWrite(bluePin, 255 - blueVal);
+
+  
+/*  cmd = Serial.readStringUntil('\r');
+  
+  if (cmd == "OFF"){
+    analogWrite(redPin,OFF);
+    analogWrite(greenPin,OFF);
+    analogWrite(bluePin,OFF);
+  }
+
+  if (cmd == "RED"){
+    analogWrite(redPin,ON);
+    analogWrite(greenPin,OFF);
+    analogWrite(bluePin,OFF);
+  }
+
+  if (cmd == "GREEN"){
+    analogWrite(redPin,OFF);
+    analogWrite(greenPin,ON);
+    analogWrite(bluePin,OFF);
+  }
+
+  if (cmd == "BLUE"){
+    analogWrite(redPin,OFF);
+    analogWrite(greenPin,OFF);
+    analogWrite(bluePin,ON);
+  }
+
+  if (cmd == "CYAN"){
+    analogWrite(redPin,OFF);
+    analogWrite(greenPin,ON);
+    analogWrite(bluePin,ON);
+  }
+
+  if (cmd == "MAGENTA"){
+    analogWrite(redPin,ON);
+    analogWrite(greenPin,OFF);
+    analogWrite(bluePin,ON);
+  }
+
+  if (cmd == "YELLOW"){
+    analogWrite(redPin,ON);
+    analogWrite(greenPin,ON);
+    analogWrite(bluePin,OFF);
+  }
+*/
+// EOF
+}
